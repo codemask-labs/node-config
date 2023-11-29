439d4c95ffff0345e7783bbb38eda3ef82d7af46
Przemyslaw Walczak <p.d.walczak@gmail.com>
2023-11-29 11:30:32 +0100

    chore: global flag for new line test regexp

--- lib/module/constants.ts
+++ lib/module/constants.ts
@@ -1,2 +1,2 @@
 export const GLOBAL_CONFIG_SERVICE_TOKEN = Symbol('GLOBAL_CONFIG_SERVICE')
-export const INCLUDES_NEW_LINE_REGEXP = new RegExp(/\n|\r/)
+export const INCLUDES_NEW_LINE_REGEXP = new RegExp(/\n|\r/, 'g')
